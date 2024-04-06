03c8a9e0210a590d6414dc02ecf2de5d192b0bd3
JeffersonCorreiax <correia_jefferson@outlook.com>
2024-04-06 13:10:49 -0300

    correcao da importacao padrao da pagina

---
+++ performancetest/src/pages/ssrpage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const SsrPage = () => {
+  return (
+    <main className=" flex-col ">
+  
+
+    <div>  TITULO </div>
+    
+    <div>  MAIN </div>
+    
+    <div>  CONTENT </div>
+    
+    <div>  CONTENT 2 </div>
+    
+    <div>  FOOTER </div>
+    
+    
+        </main>
+  ) 
+  
+};
+
+export default SsrPage;
